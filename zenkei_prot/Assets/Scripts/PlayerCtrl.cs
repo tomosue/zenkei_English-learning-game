53ae59ded509da6c25e9b459d78fc71c21661050
Tomosue Toshimasa <nikoniko04211970@gmail.com>
2020-11-18 12:06:33 +0900

    これまでの成果をcommit

---
+++ zenkei_prot/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb2d = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
