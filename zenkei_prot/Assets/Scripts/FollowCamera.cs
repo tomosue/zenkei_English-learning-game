93a7f22c74de67569612980b2576f60e9051d2dc
Tomosue Toshimasa <nikoniko04211970@gmail.com>
2020-12-08 16:04:56 +0900

    cameraにplayerを追従させる&unityを2019にupdate

---
+++ zenkei_prot/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FollowCamera : MonoBehaviour
+{
+   //寿司を格納する変数
+    public GameObject Player;
+ 
+    // Use this for initialization
+    void Start () {
+         
+    }
+     
+    // Update is called once per frame
+    void Update () {
+ 
+        Vector3 PlayerPos = Player.transform.position;
+ 
+        //カメラとプレイヤーの位置を同じにする
+        transform.position = new Vector3(PlayerPos.x, PlayerPos.y, -10);
+    }
+}
