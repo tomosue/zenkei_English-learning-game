8a00016c103710934f2b60153a1ee3fae0adeb1c
Tomosue Toshimasa <nikoniko04211970@gmail.com>
2021-04-14 15:18:21 +0900

    テキストボックスのデザインを変更 敵キャラのデザインを変更

---
+++ zenkei_prot/Assets/Scripts/TextLayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Rendering;
+using UnityEngine;
+
+ 
+public class TextLayerController : MonoBehaviour
+{
+    public int SortingOrder;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Order in Layerの数値
+        gameObject.layer = SortingOrder;
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+}
